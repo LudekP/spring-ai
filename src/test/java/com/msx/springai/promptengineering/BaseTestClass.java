598f7fbe60c8b2440fbdcbc2fa571d4bc9733eae
MISTRXPC\ludek <ludek.p@email.cz>
2024-11-03 22:26:18 +0100

    Add tests with prompt engineering.

---
+++ src/test/java/com/msx/springai/promptengineering/BaseTestClass.java
@@ -0,0 +1,22 @@
+package com.msx.springai.promptengineering;
+
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BaseTestClass {
+
+    @Autowired
+    OpenAiChatModel openAiChatClient;
+
+    String chat(String prompt) {
+        PromptTemplate promptTemplate = new PromptTemplate(prompt);
+        Prompt promptToSend = promptTemplate.create();
+
+        return openAiChatClient.call(promptToSend).getResult().getOutput().getContent();
+    }
+
+}
